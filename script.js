ad6d898fe6c7705fb2fc5f68c337f522cd206b64
vraikar-git <vijay7.raikar@gmail.com>
2022-01-18 22:28:02 +0530

    added keyboard key support

--- script.js
+++ script.js
@@ -2,12 +2,53 @@ let typedStr = "", op1, op2, opr;
 let isDotPressed = false, signToggle = false;
 
 let buttons = document.querySelectorAll(".keys");
-buttons.forEach(btn => btn.addEventListener("click",onKeyPress));
+buttons.forEach(btn => btn.addEventListener("click",onKeyClick));
+window.addEventListener("keyup",onKeyPress);
 let screen = document.querySelector("#screen");
 
+function onKeyPress(e){
+
+    console.log(e);
+    let key = document.querySelector(`button[value="${e.key}"]`);
+    
+    console.log(key);
+    if(key === null){
+        let choice;
+        switch(e.key){
+            case "Enter" :
+            case "=" :
+                choice = "equal";
+                break;
+
+            case "Backspace":
+                choice = "del";
+                break;
+
+            case "Delete":
+                choice = "clear"
+                break;
+
+            default : 
+                console.log(e.key);
+                return;
+        }
+
+        key = document.querySelector(`button[data-type="${choice}"]`);;
+        key.click();
+    }
+    else{
+        key.click();
+    }
+}
+
+function onKeyClick(e){
+    let key = e.target
+    console.log(`op1 : ${op1} op2 : ${op2} typedStr : ${typedStr} opr : ${opr}`);
+    performSwitch(key.dataset.type,key.value);
+    
+}
+
 function updateScreen(text,mode){
-
-
     switch(mode){
         case 0:
             screen.textContent = text;
@@ -20,60 +61,70 @@ function updateScreen(text,mode){
 }
 
 
-function onKeyPress(e){
-    let key = e.target
-    console.log(key.dataset.type);
-    console.log(`op1 : ${op1} op2 : ${op2} typedStr : ${typedStr} opr : ${opr}`);
+function performSwitch(selection,value){
 
-    switch(key.dataset.type){
+    console.log(`performSwitch : selection - ${selection} value - ${value}`);
+    switch(selection){
         case "digit" : 
-            typedStr += key.value;
-            
-            console.log(`typedStr : ${typedStr}`);
-            updateScreen(key.value,1);
+            typedStr += value;
+            updateScreen(value,1);
             break;
         
         case "op" :
-            updateOperands();
+            if(updateOperands()){
+                updateScreen(value,1);
+                
+            };
             if(op1 !== undefined && op2 !== undefined){
                 let result = performOperation(op1,op2,opr);
-                opr = key.value;
-                console.log(`result = ${result}`);
+                if(result === Infinity){
+                    
+                    alert("Division by Zero !!");
+                    typedStr = "";
+                    op1 = op2 = opr = undefined;
+                    isDotPressed = false;
+                    updateScreen("",0);
+                    break;
+                }
+                opr = value;
                 updateScreen(result,0);
                 typedStr = "";
                 op1 = result;
                 op2 = undefined;
+                updateScreen(value,1);
             }
-            else {
-                opr = key.value;
-            }
-            updateScreen(key.value,1);
+            opr = value;
             break;
 
         case "equal" :
-            updateOperands();
-            if(op1 !== undefined && op2 !== undefined){
+            
+            if(op1 !== undefined && op2 === undefined && typedStr != ""){
+                updateOperands();
                 let result = performOperation(op1,op2,opr);
-                console.log(`result = ${result}`);
+                if(result === Infinity){
+                    alert("Division by Zero !!");
+                    typedStr = "";
+                    op1 = op2 = opr = undefined;
+                    isDotPressed = false;
+                    updateScreen("",0);
+                    break;
+                }
                 updateScreen(result,0);
-                typedStr = result;
+                typedStr = String(result);
                 op1 = undefined;
                 op2 = undefined;
                 opr = undefined;
             }
             break;
 
-
         case "del" :
             deleteChar();
             break;
-
-        
         
         case "dot" :
             if(!isDotPressed){
-                typedStr += key.value;
-                updateScreen(key.value,1);
+                typedStr += value;
+                updateScreen(value,1);
             }
             
             isDotPressed = true;
@@ -85,35 +136,54 @@ function onKeyPress(e){
             updateScreen("",0);
             
             break;
-
-
-    }
-    console.log(`after op1 : ${op1} op2 : ${op2} typedStr : ${typedStr} opr : ${opr}`);
+        }
+        console.log(`after op1 : ${op1} op2 : ${op2} typedStr : ${typedStr} opr : ${opr}`);
 }
-
-function updateOperands(){
-
     
-    if(op1 === undefined){
+function updateOperands(){
+    if(op1 === undefined && typedStr !== ""){
         op1 = Number(typedStr);
         typedStr = "";
         isDotPressed = false;
-        return false;
+        return true;
     }
-
-    if(op1 !== undefined && op2 === undefined){
+    if(op1 !== undefined && op2 === undefined && typedStr !== ""){
         op2 = Number(typedStr);
         typedStr = "";
         isDotPressed = false;
-        console.log(`op1 : ${op1} op2 : ${op2} operator : ${opr}`);
         return true;
-    }
+}
+return false;
+}
 
-    return true;
+function deleteChar(){
+    // console.log(`deleteChar op1 : ${op1} op2 : ${op2} typedStr : ${typedStr} opr : ${opr}`);
+    if(op1 === undefined){
+        typedStr = typedStr.slice(0,-1);
+        if(isDotPressed){
+            isDotPressed = false;
+        }
+        updateScreen(typedStr,0);
+    } else if(op1 !== undefined && opr !== undefined){
+        if(typedStr === ""){
+            typedStr = String(op1);
+            updateScreen(typedStr,0);
+            opr = undefined;
+            op1 = undefined;
+
+        } else {
+            typedStr = typedStr.slice(0,-1);
+            if(isDotPressed){
+                isDotPressed = false;
+            }
+            let str = String(op1 + opr + typedStr);
+            updateScreen(str,0);
+        }
+    } 
 }
 
 function performOperation(num1, num2, opr){
-    console.log(`op1 : ${num1} op2 : ${num2} opr : ${opr}`);
+    // console.log(`op1 : ${num1} op2 : ${num2} opr : ${opr}`);
     let res ;
     switch(opr){
         case '+':
@@ -135,8 +205,4 @@ function performOperation(num1, num2, opr){
     }
     return Math.round((res + Number.EPSILON) * 100) / 100;
 
-}
-
-function deleteChar(){
-  
 }
